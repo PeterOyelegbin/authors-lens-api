5e7213db14d71fc490ade30b439cbbc890502d58
Peter Oyelegbin <peteroyelegbin@gmail.com>
2023-05-02 19:39:58 +0100

    testing stage

--- authors_lens/settings.py
+++ authors_lens/settings.py
@@ -92,20 +92,20 @@ WSGI_APPLICATION = "authors_lens.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-
-    # # Remote or production database
-    # 'default': {
-    #     'ENGINE': 'djongo',
-    #     'NAME': config('DB_NAME'),
-    #     'ENFORCE_SCHEMA': False,
-    #     'CLIENT': {
-    #         'host': config('DB_URL')
-    #     }
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
     # }
+
+    # Remote or production database
+    'default': {
+        'ENGINE': 'djongo',
+        'NAME': config('DB_NAME'),
+        'ENFORCE_SCHEMA': False,
+        'CLIENT': {
+            'host': config('DB_URL')
+        }
+    }
 }
 
 
--- blog/serializers.py
+++ blog/serializers.py
@@ -4,7 +4,23 @@ from .models import Blog, UserModel
 
 class BlogSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(slug_field="pen_name", queryset=UserModel.objects.all())
+    # author = serializers.PrimaryKeyRelatedField(read_only=True, default=serializers.CurrentUserDefault())
+    
+    # def create(self, validated_data):
+    #     blog = Blog(
+    #         author = self.context['request'].user,
+    #         menu_item_id=validated_data['menu_item_id'],
+    #         quantity=validated_data['quantity'],
+    #     )
+    #     blog.save()
+    #     return blog
 
     class Meta:
         model = Blog
         fields = "__all__"
+
+    # def create(self, validated_data):
+    #     instance = self.Meta.model(**validated_data)
+    #     instance.author = self.context['request'].user
+    #     instance.save()
+    #     return instance
