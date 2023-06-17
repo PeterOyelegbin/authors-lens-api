461fe60000bb6f4736c6a0fd28912d8bb014d889
Peter Oyelegbin <peteroyelegbin@gmail.com>
2023-06-17 11:50:36 +0100

    blog section updated

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,17 +1,8 @@
 from rest_framework import serializers
 from .models import Blog
-from accounts.serializers import SignUpSerializer
 
 
 class BlogSerializer(serializers.ModelSerializer):
-    author = SignUpSerializer(read_only=True)
-
     class Meta:
         model = Blog
         fields = "__all__"
-
-    def create(self, validated_data):
-        instance = self.Meta.model(**validated_data)
-        instance.author = self.context['request'].user
-        instance.save()
-        return instance
