cd46a0a8f02edb79e1358e0d0c2c8dd59d293311
Peter Oyelegbin <peteroyelegbin@gmail.com>
2023-06-30 10:22:53 +0100

    blog views updated

--- blog/views.py
+++ blog/views.py
@@ -14,24 +14,24 @@ class BlogView(viewsets.ModelViewSet):
     ordering_fields = ["title", "created_on"]
     search_fields = ["author", "title"]
 
-    # restrict access for create, edit, and delete to authenticated users only
-    def get_permissions(self):
-        permission_classes = []
-        if self.request.method != "GET":
-            permission_classes = [permissions.IsAuthenticated]
-        return [permission() for permission in permission_classes]
+    # # restrict access for create, edit, and delete to authenticated users only
+    # def get_permissions(self):
+    #     permission_classes = []
+    #     if self.request.method != "GET":
+    #         permission_classes = [permissions.IsAuthenticated]
+    #     return [permission() for permission in permission_classes]
 
-    # update blog owned by current authenticated user
-    @method_decorator(csrf_exempt)
-    def update(self, request, *args, **kwargs):
-        if self.queryset.filter(author=self.request.user):
-            return super().update(request, *args, **kwargs)
-        else:
-            return Response({"Message": "Permission required."}, 401)
+    # # update blog owned by current authenticated user
+    # @method_decorator(csrf_exempt)
+    # def update(self, request, *args, **kwargs):
+    #     if self.queryset.filter(author=self.request.user):
+    #         return super().update(request, *args, **kwargs)
+    #     else:
+    #         return Response({"Message": "Permission required."}, 401)
 
-    # delete blog owned by current authenticated user
-    def destroy(self, request, *args, **kwargs):
-        if self.queryset.filter(author=self.request.user):
-            return super().destroy(request, *args, **kwargs)
-        else:
-            return Response({"Message": "Permission required."}, 401)
+    # # delete blog owned by current authenticated user
+    # def destroy(self, request, *args, **kwargs):
+    #     if self.queryset.filter(author=self.request.user):
+    #         return super().destroy(request, *args, **kwargs)
+    #     else:
+    #         return Response({"Message": "Permission required."}, 401)
