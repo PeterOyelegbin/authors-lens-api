db120f31a3a8da576c114a290b1aeb7caa153561
Peter Oyelegbin <peteroyelegbin@gmail.com>
2023-07-02 14:12:16 +0100

    cors settings

--- authors_lens/settings.py
+++ authors_lens/settings.py
@@ -31,9 +31,9 @@ ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app"]
 
 # CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = ["localhost:*", "127.0.0.1:*", "https://*.vercel.app", "https://*.netlify.app"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:5173", "http://127.0.0.1:*", "https://*.vercel.app", "https://*.netlify.app"]
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost", "http://127.0.0.1", "https://*.vercel.app", "https://*.netlify.app"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:5173", "http://127.0.0.1:*", "https://*.vercel.app", "https://*.netlify.app"]
 
 # Application definition
 
