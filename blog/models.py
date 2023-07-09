a15fe306c66a3a6cec3cb5209273c1eb1c8ee1ac
Peter Oyelegbin <peteroyelegbin@gmail.com>
2023-07-09 17:49:10 +0100

    blog category updated

--- blog/models.py
+++ blog/models.py
@@ -5,19 +5,12 @@ from ckeditor.fields import RichTextField
 
 
 # Create your models here.
-category_option = [
-    ("Technology", "Technology"),
-    ("Lifestyle", "Lifestyle"),
-    ("Relationship", "Relationship"),
-    ("Others", "Others"),
-]
-
 class Blog(models.Model):
     id = models.UUIDField(default=uuid4, unique=True, primary_key=True, editable=False)
     author = models.ForeignKey(UserModel, on_delete=models.CASCADE)
     title = models.CharField(max_length=225, unique=True, db_index=True)
     cover_image = models.ImageField(upload_to="authorslens/images/")
-    category = models.CharField(max_length=20, choices=category_option, default="Others")
+    category = models.CharField(max_length=100)
     content = RichTextField()
     created_on = models.DateField(auto_now_add=True)
     updated_on = models.DateField(auto_now=True)
@@ -27,4 +20,4 @@ class Blog(models.Model):
         ordering = ["-updated_on"]
 
     def __str__(self):
-        return f"{self.title} || {self.author}"
+        return f"{self.title}"
